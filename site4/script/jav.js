230b9d0cfd68cc44a90ce978d669944feb888ae5
Eboma Peter <ebomatunde@gmail.com>
2022-03-19 17:17:45 +0100

    'work'

---
+++ site4/script/jav.js
@@ -0,0 +1,8 @@
+function openSlideMenu(){
+document.getElementById('close-btn').style.width = '250px';
+document.getElementById('main').style.marginLeft = '250px';
+}
+function closeSlideMenu(){
+document.getElementById('close-btn').style.width ='0';
+document.getElementById('main').style.marginLeft ='0';
+}
